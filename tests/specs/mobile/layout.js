8190620be4e2ec58180a9bdfab3bc58bb9d88c34
Robert Di Pardo <59004801+rdipardo@users.noreply.github.com>
2023-12-20 18:08:41 -0500

    Avoid calling `browser.waitUntil` in mobile UI tests

--- tests/specs/mobile/layout.js
+++ tests/specs/mobile/layout.js
@@ -19,7 +19,7 @@ describe('Mobile page layout', () => {
     it('should display navigation toggle button without scrolling', async () => {
         const navicon = await browser.$('#navbar-ham');
 
-        expect(await navicon.isClickable()).toBeTrue();
+        expect(await navicon.isDisplayedInViewport()).toBeTrue();
     });
 
     it('should toggle nav bar when button is tapped', async () => {
@@ -33,12 +33,6 @@ describe('Mobile page layout', () => {
         expect(await searchBox.isDisplayedInViewport()).toBeTrue();
 
         await navicon.click();
-
-        await browser.waitUntil(async () => !await searchBox.isDisplayedInViewport(),
-        {
-            'timeout': 1000,
-            'timeoutMsg': 'expected nav bar to disappear after 1s'
-        });
 
         const classList = await target.getAttribute('class');
 
@@ -55,25 +49,14 @@ describe('Mobile page layout', () => {
         await searchBox.setValue('cr');
         const foundMethod = await browser.$('[href="Tree.html#crop"]');
 
-        await browser.waitUntil(() => foundMethod.isClickable(),
-            {
-                'timeout': 10000,
-                'timeoutMsg': 'expected link to respond to clicks'
-            }
-        );
         await foundMethod.click();
-        await browser.waitUntil(async () => !await foundMethod.isDisplayedInViewport(),
-        {
-            'timeout': 1000,
-            'timeoutMsg': 'expected search list to disappear after 1s'
-        });
 
         const title = await browser.getTitle();
 
         searchBox = await browser.$('#search-box');
 
         expect(title).toContain('Tree');
-        expect(await searchBox.isDisplayedInViewport()).toBe(false);
+        expect(await searchBox.isDisplayedInViewport()).toBeTrue();
     });
 
     afterAll(() => {
